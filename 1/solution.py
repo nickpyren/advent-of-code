33c2a70325019d0dbfdcb9d575f59e437ef2ed68
Nick Pyren <nick.pyren@gmail.com>
2018-12-02 21:59:50 -0500

    Initial commits

---
+++ 1/solution.py
@@ -0,0 +1,25 @@
+filename = "input"
+value = 0
+occurences = dict()
+occurences[0] = 1
+while not (2 in occurences.values()):
+    with open(filename,'r') as file:
+        for line in file:
+            operator = line[0]
+            operand = int(line[1::])
+
+            if operator == '+':
+                value += operand
+
+            elif operator == '-':
+                value -= operand
+
+            if value in occurences:
+                occurences[value] += 1
+                if occurences[value] == 2:
+                    break
+            
+            else:
+                occurences[value] = 1
+
+print(value)
---
+++ 2/solution.py
@@ -0,0 +1,4 @@
+filename = "input"
+file = open(filename,'r')
+for line in file:
+    print(line)
